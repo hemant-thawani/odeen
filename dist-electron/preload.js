e8e83c751b522691ea2373a838a9a14329f4c71e
hemant-thawani <hemantthawani57@gmail.com>
2026-01-08 22:47:55 +0530

    initial odeen commit

---
+++ dist-electron/preload.js
@@ -0,0 +1,17 @@
+"use strict";
+const { contextBridge, ipcRenderer } = require("electron");
+contextBridge.exposeInMainWorld("electron", {
+  // Example API - expose only what you need
+  send: (channel, data) => {
+    const validChannels = ["toMain"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    const validChannels = ["fromMain"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.on(channel, (event, ...args) => func(...args));
+    }
+  }
+});
